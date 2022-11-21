0a3fd32fd7da66108fffbcc54c29bf28d53600c0
cassel23 <fadhillahpuspitasari@gmail.com>
2022-11-21 10:08:13 +0700

    ubahh

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,6 +4,12 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
+    private $userModel;
+
+    public function __construct() {
+        $this->userModel = new \App\Models\UserModel();
+    }
+    
     public function index()
     {
         return view('welcome_message');
