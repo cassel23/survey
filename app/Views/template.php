6daaa5e94a1e48fab9c358ddc71ce73b35d75d11
diindf <dinda.felicia28@gmail.com>
2022-11-21 08:47:40 +0700

    ubah dist

--- app/Views/template.php
+++ app/Views/template.php
@@ -104,7 +104,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="/assets/dist/img/ShuyoSuvey_logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="/assets/_dist/img/ShuyoSuvey_logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">ShuyoSurvey</span>
     </a>
 
@@ -113,7 +113,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="/assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="/assets/_dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Mark Lee</a>
@@ -225,6 +225,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <!-- Bootstrap 4 -->
 <script src="/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="/assets/dist/js/adminlte.min.js"></script>
+<script src="/assets/_dist/js/adminlte.min.js"></script>
 </body>
 </html>
