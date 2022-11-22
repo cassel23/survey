9d54111a63b0040de83e9df49a193ec5ef2963a4
diindf <dinda.felicia28@gmail.com>
2022-11-22 15:34:35 +0700

    bikin model buat survey tapi salah

---
+++ app/Models/SurveyModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class SurveyModel extends Model
+{
+    protected $table      = 'survey';
+    protected $allowedFields = ['title', 'description'];
+}
