6f7a6fc31778d8bf6d1ff099dc251e09fb3245ac
diindf <dinda.felicia28@gmail.com>
2022-11-22 10:33:37 +0700

    tambahin t

--- app/Views/user.php
+++ app/Views/user.php
@@ -24,7 +24,7 @@
 <div class="login-box">
   <div class="login-logo">
   <a class="brand-link">
-    <img src="/assets/_dist/img/ShuyoSuvey_logo.png" class="img-circle elevation-2" alt="User Logo" style="width : 150px;heigh : 150px;"> 
+    <img src="/assets/_dist/img/ShuyoSuvey_logo.png" class="img-circle elevation-2" alt="User Logo" style="width : 150px;height : 150px;"> 
     <a href="../../index2.html"><b>Shuyo</b>Survey</a>
   </div>
 
