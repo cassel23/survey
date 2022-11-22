15b1049c36365146aece98a83aa21c2d83836e9f
diindf <dinda.felicia28@gmail.com>
2022-11-22 10:31:25 +0700

    buang link logo

--- app/Views/user.php
+++ app/Views/user.php
@@ -23,7 +23,7 @@
   <center>
 <div class="login-box">
   <div class="login-logo">
-  <a href="index3.html" class="brand-link">
+  <a class="brand-link">
     <img src="/assets/_dist/img/ShuyoSuvey_logo.png" class="img-circle elevation-2" alt="User Logo" style="width : 150px;heigh : 150px;"> 
     <a href="../../index2.html"><b>Shuyo</b>Survey</a>
   </div>
