9d97eb5cdb678270ae602d4bc3e610dd09365c99
cassel23 <fadhillahpuspitasari@gmail.com>
2022-11-24 21:49:44 +0700

    dash and temp

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -16,20 +16,28 @@
               <div class="card-body">
                 
                     <div class="card bg-gradient-info">
-                    <div class="card-header border-0">
-                        <h3 class="card-title">
+                        <div class="row">
+                            <div class="col-6 p-3">
+                            <h3 class="card-title">
                         <i class="fas fa-th mr-1"></i>
                         Sample Survey
-                        </h3>    
-                    </div>
-
-                    <div class="card-header border-0 ml-auto">
-                            <div class="input-group">
-                                <button type="button" class="btn btn-default float-left" id="daterange-btn">
+                        </h3>
+                            </div>
+                            <div class="col-6 p-3">
+                            <div class="input-group ">
+                                <button type="button" class="btn btn-default ml-auto" id="daterange-btn">
                                 <i class="far fa-calendar-alt"></i> Date range picker
                                 <i class="fas fa-caret-down"></i>
                                 </button>
                             </div>
+                            </div>
+                        </div>
+                    <div class="card-header border-0">
+                            
+                    </div>
+
+                    <div class="card-header border-0 ml-auto">
+                            
                      </div>
 
                     <div class="card-body">
@@ -58,57 +66,6 @@
             <!-- /.card -->
           </div>
 </div>
-
-<!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- Select2 -->
-<script src="../../plugins/select2/js/select2.full.min.js"></script>
-<!-- Bootstrap4 Duallistbox -->
-<script src="../../plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
-<!-- InputMask -->
-<script src="../../plugins/moment/moment.min.js"></script>
-<script src="../../plugins/inputmask/jquery.inputmask.min.js"></script>
-<!-- date-range-picker -->
-<script src="../../plugins/daterangepicker/daterangepicker.js"></script>
-<!-- bootstrap color picker -->
-<script src="../../plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js"></script>
-<!-- Tempusdominus Bootstrap 4 -->
-<script src="../../plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-<!-- Bootstrap Switch -->
-<script src="../../plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
-<!-- BS-Stepper -->
-<script src="../../plugins/bs-stepper/js/bs-stepper.min.js"></script>
-<!-- dropzonejs -->
-<script src="../../plugins/dropzone/min/dropzone.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="../../dist/js/demo.js"></script>
-<!-- Page specific script -->
-
-<script>
-                            $(function () {
-                            //Date range as a button
-                                $('#daterange-btn').daterangepicker(
-                                {
-                                    ranges   : {
-                                    'Today'       : [moment(), moment()],
-                                    'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-                                    'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
-                                    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-                                    'This Month'  : [moment().startOf('month'), moment().endOf('month')],
-                                    'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-                                    },
-                                    startDate: moment().subtract(29, 'days'),
-                                    endDate  : moment()
-                                },
-                                function (start, end) {
-                                    $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
-                                }
-                                )
-                            </script>
 
 </html>
 <?= $this->endSection() ?>
--- app/Views/template.php
+++ app/Views/template.php
@@ -16,6 +16,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <link rel="stylesheet" href="/assets/plugins/fontawesome-free/css/all.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="/assets/_dist/css/adminlte.min.css">
+
+  <link rel="stylesheet" href="/assets/plugins/daterangepicker/daterangepicker.css">
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
@@ -23,21 +25,21 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
-    <ul class="navbar-nav">
+    <ul class="navbar-nav" style="font-family:serif;">
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="http://localhost:8080/dashboard" class="nav-link fa fa-dashboard">
-          <i class="ml-1"></i>Dashboard</a>
+        <a href="http://localhost:8080/dashboard" class="nav-link">
+          <i class="ml-1 fas fa-tachometer-alt"></i>Dashboard</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="http://localhost:8080/mysurvey" class="nav-link fa fa-newspaper-o">
-        <i class="ml-1"></i>My Surveys</a>
+        <a href="http://localhost:8080/mysurvey" class="nav-link">
+        <i class="ml-1 fas fa-newspaper"></i>My Surveys</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="http://localhost:8080/resultsurvey" class="nav-link fa fa-bar-chart-o">
-        <i class="ml-1"></i>Result Surveys</a>
+        <a href="http://localhost:8080/resultsurvey" class="nav-link">
+        <i class="ml-1 fas fa-chart-bar"></i>Result Surveys</a>
       </li>
     </ul>
 
@@ -116,23 +118,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
     <!-- Sidebar -->
     <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        
-     
-      </div>
-
-      <!-- SidebarSearch Form -->
-      <div class="form-inline">
-        
-      </div>
+  
 
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item menu-open">
+          <li class="nav-item menu-open border-0">
             <a href="http://localhost:8080/" class="nav-link active">
               <i class="nav-icon fas fa-home"></i>
               <p>
@@ -198,5 +191,42 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <script src="/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="/assets/_dist/js/adminlte.min.js"></script>
+<script src="/assets/plugins/moment/moment.min.js"></script>
+<script src="/assets/plugins/daterangepicker/daterangepicker.js"></script>
+<script>
+  $('#daterange-btn').daterangepicker(
+
+{
+
+  ranges   : {
+
+    'Today'       : [moment(), moment()],
+
+    'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+
+    'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
+
+    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+
+    'This Month'  : [moment().startOf('month'), moment().endOf('month')],
+
+    'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+
+  },
+
+  startDate: moment().subtract(29, 'days'),
+
+  endDate  : moment()
+
+},
+
+function (start, end) {
+
+  $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
+
+}
+
+)
+</script>
 </body>
 </html>
