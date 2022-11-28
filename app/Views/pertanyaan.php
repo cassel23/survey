106468ee432e8d7ab7ce8c4916b0acb71186f8ef
diindf <dinda.felicia28@gmail.com>
2022-11-28 13:26:25 +0700

    viewwwww pertanyaan

---
+++ app/Views/pertanyaan.php
@@ -0,0 +1,17 @@
+<?= $this->extend("template") ?>
+
+<?= $this->section("content") ?>
+<center>
+<section class="content mx-5">
+    <div class="card ">
+    <div class="card-body home-card-body justify-content-center">
+    <div class="row justify-content-center">
+        
+
+    </div>
+    </div>
+    </div>
+</section>
+</center>
+
+<?= $this->endSection() ?>
