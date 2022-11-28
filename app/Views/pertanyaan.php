81bf0ebfbe425117ed574e8c4db515677284099a
cassel23 <fadhillahpuspitasari@gmail.com>
2022-11-28 14:16:02 +0700

    card question

--- app/Views/pertanyaan.php
+++ app/Views/pertanyaan.php
@@ -1,17 +1,26 @@
 <?= $this->extend("template") ?>
 
 <?= $this->section("content") ?>
-<center>
-<section class="content mx-5">
-    <div class="card ">
-    <div class="card-body home-card-body justify-content-center">
-    <div class="row justify-content-center">
-        
 
-    </div>
-    </div>
-    </div>
-</section>
-</center>
+<!DOCTYPE html>
+  <html lang="en">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1", shrink-to-fit=no>
+    <link rel="shortcut icon" type="image/png" href="/assets/_dist/img/ShuyoSuvey_logo.png"/>
+    <title>ShuyoSurvey | Survey Question</title>
+
+            <center>
+            <section class="content mx-5">
+                <div class="card" style="background-color:lightsteelblue; width: 1100px;height: 650px;">
+                    <div class="card-body home-card-body justify-content-center">
+                        <div class="row justify-content-center">
+                    
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+            </center>
+  </html>
 
 <?= $this->endSection() ?>
