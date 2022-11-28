09f8fcf4cf6f5a20333cafda292a36fbd1844666
cassel23 <fadhillahpuspitasari@gmail.com>
2022-11-28 08:57:46 +0700

    hapus logo di template

--- app/Views/template.php
+++ app/Views/template.php
@@ -5,11 +5,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 -->
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="shortcut icon" type="image/png" href="/assets/_dist/img/ShuyoSuvey_logo.png"/>
-  <title>ShuyoSurvey | </title>
-
+  
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
