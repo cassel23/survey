370daa4fa6d5bff3efd08b1eba51f23a5e00074f
cassel23 <fadhillahpuspitasari@gmail.com>
2022-11-28 11:57:04 +0700

    undo rsltsurv and home hehew

--- app/Views/resultsurvey.php
+++ app/Views/resultsurvey.php
@@ -11,15 +11,12 @@
 
             <label>Nama Survey</label>
             <select class="form-control select2 col-3" style="width: 100%;">
-            <?php foreach($survey as $val) : ?>
-            <option><?= $val['title'] ?></option>
             <option>Alaska</option>
             <option>California</option>
             <option>Delaware</option>
             <option>Tennessee</option>
             <option>Texas</option>
             <option>Washington</option>
-            <?php endforeach; ?>
             </select>
             </div>
           
